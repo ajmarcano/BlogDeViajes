88255da9655a07f63a64f8e0b8875b77bf62716b
ajmarcano <aj.marcanobarrios@gmail.com>
2022-05-26 19:00:43 -0300

    creado template para agregar.html

--- ProyectoBlog/ProyectoBlog/urls.py
+++ ProyectoBlog/ProyectoBlog/urls.py
@@ -14,12 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from BlogViajes import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('principal/', views.principal, name="principal"),
-    path('agregar', views.agregar, name="agregar"),
-    path('buscar', views.buscar, name="buscar"),
+    path('BlogViajes/', include('BlogViajes.urls'))
 ]
