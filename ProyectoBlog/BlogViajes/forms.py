e7b019b81ac68aa5dd5e25e4f2fa408dcb1ee431
ajmarcano <aj.marcanobarrios@gmail.com>
2022-05-26 20:05:34 -0300

    agregado form de agregar entradas

--- ProyectoBlog/BlogViajes/forms.py
+++ ProyectoBlog/BlogViajes/forms.py
@@ -4,5 +4,5 @@ class Entrada(forms.Form):
     destino = forms.CharField(label="Destino", max_length=100)
     imagen = forms.URLField(label="Imagen")
     reseña = forms.CharField(label="Reseña", max_length=100)
-    autor = forms.Charfield(label="Autor", max_length=100)
+    autor = forms.CharField(label="Autor", max_length=100)
  
--- ProyectoBlog/BlogViajes/views.py
+++ ProyectoBlog/BlogViajes/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from BlogViajes.forms import Entrada
 
 # Create your views here.
 
@@ -6,7 +7,29 @@ def principal(request):
     return render(request, "principal.html")
 
 def agregar(request):
-    return render(request, "agregar.html")
+
+    if request.method == "POST":
+
+        formulario = Entrada(request.POST)
+
+        print(formulario)
+
+        if formulario.is_valid:
+
+            informacion = formulario.cleaned_data
+
+            entrada = Entrada ( destino=informacion["destino"], imagen=informacion["imagen"], reseña=informacion["reseña"], autor=informacion["autor"] )
+
+            entrada.save()
+
+            return render(request, r"C:\Users\Leisa\Downloads\ProyectoFinal-20220526T004057Z-001\ProyectoFinal\ProyectoBlog\BlogViajes\templates\principal.html")
+
+    else:
+        formulario = Entrada()
+
+    return render(request, r"C:\Users\Leisa\Downloads\ProyectoFinal-20220526T004057Z-001\ProyectoFinal\ProyectoBlog\BlogViajes\templates\agregar.html", {"formulario":formulario})
 
 def buscar(request):
     return render(request, "buscar.html")
+    
+    
